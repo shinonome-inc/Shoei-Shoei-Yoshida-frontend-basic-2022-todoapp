7900738f10725397f813ab8dd525d3fa78fd8b9b
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-15 22:55:52 +0900

    Atoms/Titleの大枠を作成

---
+++ src/components/Atoms/Title/index.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import React from "react";
+
+export const Title = () => {
+  return <StyledText>SIMPLE TODO APP</StyledText>;
+};
+
+const StyledText = styled.div`
+  color: white;
+`;
