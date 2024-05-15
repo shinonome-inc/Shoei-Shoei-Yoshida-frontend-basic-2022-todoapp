c8b1427fd66ad65935f77d5d05920278e61cd58d
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-15 23:19:20 +0900

    フォントを調整

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import COLOR from "../../../variables/color.js";
+import FONTFAMILY from "../../../variables/font_family.js";
 import TEXT from "../../../variables/texts.js";
 import React from "react";
 
@@ -9,5 +10,6 @@ export const Title = () => {
 
 const StyledText = styled.div`
   color: ${COLOR.WHITE};
+  font-family: ${FONTFAMILY.ROBOTO};
   font-size: ${TEXT.L};
 `;
