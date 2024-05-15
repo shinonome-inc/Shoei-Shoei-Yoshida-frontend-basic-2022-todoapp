ea9caac4380ddc32291bf83a0222ea9d6fc6c049
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-15 23:24:55 +0900

    モバイルのときの表示を追加（レスポンシブ対応）

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import BREAKPOINT from "../../../variables/breakpoint.js";
 import COLOR from "../../../variables/color.js";
 import FONTFAMILY from "../../../variables/font_family.js";
 import TEXT from "../../../variables/texts.js";
@@ -12,4 +13,8 @@ const StyledText = styled.div`
   color: ${COLOR.WHITE};
   font-family: ${FONTFAMILY.ROBOTO};
   font-size: ${TEXT.L};
+  @media (max-width: ${BREAKPOINT.MEDIUM}) {
+    font-family: ${FONTFAMILY.NOTO_SANS};
+    font-size: ${TEXT.M};
+  }
 `;
