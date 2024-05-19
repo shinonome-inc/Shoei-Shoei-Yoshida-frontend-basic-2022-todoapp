f324411d12d51b93ee3a3adfdce601d56fd282f4
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-19 12:19:39 +0900

    font-familyの書き方を修正

--- src/components/Atoms/Title/index.jsx
+++ src/components/Atoms/Title/index.jsx
@@ -1,9 +1,9 @@
+import React from "react";
 import styled from "styled-components";
 import BREAKPOINT from "../../../variables/breakpoint.js";
 import COLOR from "../../../variables/color.js";
 import FONTFAMILY from "../../../variables/font_family.js";
 import TEXT from "../../../variables/texts.js";
-import React from "react";
 
 export const Title = () => {
   return <StyledText>SIMPLE TODO APP</StyledText>;
@@ -12,9 +12,9 @@ export const Title = () => {
 const StyledText = styled.div`
   color: ${COLOR.WHITE};
   font-family: ${FONTFAMILY.ROBOTO};
-  font-size: ${TEXT.L};
+  ${TEXT.L};
   @media (max-width: ${BREAKPOINT.MEDIUM}) {
     font-family: ${FONTFAMILY.NOTO_SANS};
-    font-size: ${TEXT.M};
+    ${TEXT.M};
   }
 `;
