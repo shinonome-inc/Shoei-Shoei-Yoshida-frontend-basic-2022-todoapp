328ac4424c55acd2a8df811eed0c9787be3dc1da
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-26 10:48:33 +0900

    storybookに表示させる

--- src/components/Atoms/EditButton/index.stories.jsx
+++ src/components/Atoms/EditButton/index.stories.jsx
@@ -1,1 +1,12 @@
 import EditButton from "./index";
+
+export default {
+  title: "Atoms/EditButton",
+  component: EditButton,
+};
+
+export const Default = {
+  args: {
+    onClick: () => console.log("clicked"),
+  },
+};
