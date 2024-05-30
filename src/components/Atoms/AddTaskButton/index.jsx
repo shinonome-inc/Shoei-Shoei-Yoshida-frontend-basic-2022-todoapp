2e20f370cd5915d752b06d89fdd254321a3b23c8
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-30 17:40:22 +0900

    必要な素材をimportする

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+import plus from "../../../assets/svg/plus.svg";
+import COLOR from "../../../variables/color.js";
+import FONTFAMILY from "../../../variables/font_family.js";
+import TEXT from "../../../variables/texts.js";
+
