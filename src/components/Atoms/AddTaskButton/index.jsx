5365e69b86249e5436a270e720b8af1f7bb78d80
shoei03 <shoei.yoshida.03@gmail.com>
2024-05-30 17:41:40 +0900

    ボタンのCSSを定義

--- src/components/Atoms/AddTaskButton/index.jsx
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -1,4 +1,27 @@
-import React from "react";
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  padding: 2px 6px;
+  width: 126px;
+  height: 24px;
+  background: transparent;
+  border: none;
+  border-radius: 12px;
+  cursor: pointer;
+  position: relative;
+  &:hover::before {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+    background: ${COLOR.GREEN};
+    opacity: 0.2;
+  }
+`;
 import styled from "styled-components";
 import plus from "../../../assets/svg/plus.svg";
 import COLOR from "../../../variables/color.js";
