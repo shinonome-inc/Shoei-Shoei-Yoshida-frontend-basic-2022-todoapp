e88dbef9b2f95cd2795745c6e7cce1cf15399104
shoei03 <shoei.yoshida.03@gmail.com>
2024-06-12 11:53:41 +0900

    CSSを適用して、形を整形

--- src/components/Atoms/Checkbox/index.jsx
+++ src/components/Atoms/Checkbox/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 import check from "../../../assets/svg/check.svg";
+import COLOR from "../../../variables/color.js";
 
 const Button = styled.button`
-
+    background-color: transparent;
+    border: ${COLOR.LIGHT_GRAY} solid 2px;
+    width: 20px;
+    height: 20px;
+    border-radius: 2px;
+    padding: 2px;
+    cursor: pointer;
 `;
 
 const Icon = styled.img`
