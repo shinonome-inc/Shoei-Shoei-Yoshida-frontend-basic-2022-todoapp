9f4e5ae85d1fd561494e5bb8b44fc57784de6db6
shoei03 <shoei.yoshida.03@gmail.com>
2024-06-15 15:25:15 +0900

    ボックスの中を黒に設定

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import COLOR from "../../../variables/color.js";
 
 const Input = styled.input`
     width: 100%;
     height: 20px;
     padding: 0px 4px;
+    border-radius: 2px;
+    background-color: ${COLOR.BLACK};
 `;
 
 const InputComponent = ({ onClick }) => {
