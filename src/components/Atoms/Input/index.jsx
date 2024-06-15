d755a5d65c35a24f68ee2c83d9b5bfe4693dfec4
shoei03 <shoei.yoshida.03@gmail.com>
2024-06-15 15:22:19 +0900

    Inputを親要素いっぱいに広げた

--- src/components/Atoms/Input/index.jsx
+++ src/components/Atoms/Input/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const Input = styled.input`
+    width: 100%;
+    height: 20px;
+    padding: 0px 4px;
 `;
 
 const InputComponent = ({ onClick }) => {
